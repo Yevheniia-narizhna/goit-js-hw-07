24e09f5ed1c7605a7702a4c31a3cc6021117b7ff
Yevheniia-narizhna <162164893+Yevheniia-narizhna@users.noreply.github.com>
2024-06-18 19:17:51 +0300

    four

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,20 @@
+const formReg = document.querySelector(".login-form");
+formReg.addEventListener("submit", checkSubmit);
 
+function checkSubmit(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+
+  if (!email || !password) {
+    alert("All form fields must be filled in");
+    return;
+  }
+  const userInfo = {
+    [form.elements.email.name]: email.trim(),
+    [form.elements.password.name]: password.trim(),
+  };
+  console.log(userInfo);
+  formReg.reset();
+}
